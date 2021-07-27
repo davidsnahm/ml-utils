9bfff51c05a20f071b353fac4bdfa30f26017df5
davidsnahm <davidsnahm@gmail.com>
2021-07-26 20:53:29 -0700

    adding stochastic GD

--- optimizers/gradient_descent.py
+++ optimizers/gradient_descent.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def run(X, y, loss_fn="MSE", lr=0.1, num_iter=1000):
+def run(X, y, loss_fn="MSE", lr=0.1, num_iter=1000, batch_size=None):
+    if not batch_size:
+        batch_size = X.shape[0]
     w = np.random.randn(X.shape[1])
     grad_fn = get_gradient(loss_fn)
     for _ in range(num_iter):
-        step = lr * grad_fn(X, y, w)
+        batch = X[np.random.choice(X.shape[0], batch_size, replace=False), :]
+        step = lr * grad_fn(batch, y, w)
         w = w - step
         if abs(step) < 0.001:
             break
